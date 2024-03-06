19ab683adec0dbbebde2d8f9d73324e7b657ef7f
ayrton chaves <ayrton.chaves@edu.univ-fcomte.fr>
2024-03-06 11:17:55 +0100

    Label personnalisé par "slot"

--- src/components/FormBasket.vue
+++ src/components/FormBasket.vue
@@ -34,7 +34,20 @@ const chaussure = ref<Basket>({...props.data} ?? {});
 <FormKit type="form" v-model="chaussure">
 <FormKit name="semelle" label="semelle" value="#FFFFFF" type="radio" :options="colors" />
 <FormKit name="empeigne" label="empeigne" value="#FFFFFF" type="radio" :options="colors" />
-<FormKit name="pointe" label="pointe" value="#FFFFFF" type="radio" :options="colors" />
+
+<FormKit name="pointe" label="pointe" value="#FFFFFF" type="radio" :options="colors">
+
+<template #label="context">
+<div
+class="h-6 w-6 rounded-full"
+:style="{ backgroundColor: context.option.value }">
+</div>
+
+<span>{{ context.option. label }}</span>
+</template>
+
+</FormKit>
+
 <FormKit name="oeillet" label="oeillet" value="#FFFFFF" type="radio" :options="colors" />
 <FormKit name="bande" label="bande" value="#FFFFFF" type="radio" :options="colors" />
 <FormKit name="languette" label="languette" value="#FFFFFF" type="radio" :options="colors" />
